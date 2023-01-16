2bf689d675bdf0dad36dfc12d287acff7fdf20a6
hj0302 <mhjong3213@gmail.com>
2023-01-17 01:02:37 +0900

    v1 commit

---
+++ slack_bot.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+channel_id="stock-auto"
+message=''
+
+
+def send_to_slack(channel_id, message):
+    token="xoxb-1549346898082-4649699744162-JWt8vbjQmbj4Vw4LXW3bIpdX"
+    client = WebClient(token=token)
+
+    now = datetime.now().strftime('[%Y/%m/%d %H:%M:%S] ')
+
+    try :
+        response = client.chat_postMessage(channel=channel_id, text=f'{now} - {message}')
+    except SlackApiError as e:
+        print(f'Error: {e.response.error}')
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="SlackBot")
+    
+    parser.add_argument("-c", "--channel_id", help="Enter the channel ID you want to send")
+    parser.add_argument("-m", "--message", help="Enter the Message you want to send")
+    
+    args = parser.parse_args()
+
+    send_to_slack(args.channel_id, args.message)
